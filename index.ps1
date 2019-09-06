39fe2ddbf52b4b38a0a13290ef933d510a94a1b6
vinzentius-it-admin <it@vinzentius.de>
2019-09-06 09:35:27 +0200

    use deskriptiv notation for negation

--- index.ps1
+++ index.ps1
@@ -9,7 +9,7 @@ Param (
 $configFilePath = "config.json"
 $configDistFilePath = "config.json.dist"
 $checkFile = Test-Path $configFilePath
-if (!$checkFile) {
+if (-not $checkFile) {
     Write-Host "Die Konfigurationsdatei $configFilePath konnte nicht gefunden werden. Bitte erstellen Sie eine Konfigurationsdatei mit dem Dateinamen $configFilePath im Wurzelverzeichnis dieser Anwendung. Dafür kann die Datei $configDistFilePath, die ebenfalls im Wurzelverzeichnis liegt, als Vorlage verwendet werden."
     exit 0
 }
