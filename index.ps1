483cf502e3410ac4252063b561f7ec6d94e7a49f
vinzentius-it-admin <it@vinzentius.de>
2019-09-06 08:49:48 +0200

    remove debug dump

--- index.ps1
+++ index.ps1
@@ -13,8 +13,3 @@ if (!$checkFile) {
     Write-Host "Die Konfigurationsdatei $configFilePath konnte nicht gefunden werden. Bitte erstellen Sie eine Konfigurationsdatei mit dem Dateinamen $configFilePath im Wurzelverzeichnis dieser Anwendung. Dafür kann die Datei $configDistFilePath, die ebenfalls im Wurzelverzeichnis liegt, als Vorlage verwendet werden."
     exit 0
 }
-
-$linuxfile = Get-Content -Raw -Path $configFilePath | ConvertFrom-Json
-foreach ($obj in $linuxfile.patterns[0].value) {
-    Write-Host ("Got: " + $obj)
-}
